31883083f34f8b4495f70e731280fe36bbc33706
Nathanael Wong <natgeo.wong@outlook.com>
2021-03-12 02:49:45 -0500

    If vertical AND pressure are specified, use pressure to calculate TABS from THETA.

--- forcing.f90
+++ forcing.f90
@@ -11,9 +11,9 @@ implicit none
 
 integer i,j,k,n,nn,m,iz,iday0,iday
 real coef, radtend, dayy
-real tt(nzm,2),qq(nzm,2),uu(nzm,2),vv(nzm,2),ww(nzm,2)
+real tt(nzm,2),qq(nzm,2),uu(nzm,2),vv(nzm,2),ww(nzm,2),pp
 real ratio1, ratio2, ratio_t1, ratio_t2
-logical zgrid
+logical zgrid, pgrid
 
 real Derbyshire_qv(nzm), Derbyshire_theta(nzm)
 real hadvs(nzm), hadvq(nzm), tmp_hadvq(nzm)
@@ -52,34 +52,48 @@ call t_startf ('forcing')
     m = nn+n-1
     if(zsnd(2,m).gt.zsnd(1,m)) then
       zgrid=.true.
-    else if(psnd(2,m).lt.psnd(1,m)) then
-      zgrid=.false.
     else
+      zgrid=.false.
+    endif
+
+    if(psnd(2,m).lt.psnd(1,m)) then
+      pgrid=.true.
+    else
+      pgrid=.false.
+    end if
+
+    if((.not.zgrid).and.(.not.pgrid)) then
       if(masterproc) print*,'error in grid in snd'
       stop
     end if
+
     do iz = 1,nzm
       if(zgrid) then
         do i = 2,nzsnd
           if(z(iz).le.zsnd(i,m)) then
-           coef = (z(iz)-zsnd(i-1,m))/(zsnd(i,m)-zsnd(i-1,m))
-           tt(iz,n)=tsnd(i-1,m)+(tsnd(i,m)-tsnd(i-1,m))*coef
-           tt(iz,n)=tt(iz,n)/prespoti(iz)
-           qq(iz,n)=qsnd(i-1,m)+(qsnd(i,m)-qsnd(i-1,m))*coef
-           uu(iz,n)=usnd(i-1,m)+(usnd(i,m)-usnd(i-1,m))*coef
-           vv(iz,n)=vsnd(i-1,m)+(vsnd(i,m)-vsnd(i-1,m))*coef
+            coef = (z(iz)-zsnd(i-1,m))/(zsnd(i,m)-zsnd(i-1,m))
+            tt(iz,n)=tsnd(i-1,m)+(tsnd(i,m)-tsnd(i-1,m))*coef
+            if(pgrid) then
+              pp=psnd(i-1,m)+(psnd(i,m)-psnd(i-1,m))*coef
+              tt(iz,n)=tt(iz,n)/((1000./pp)**(rgas/cp))
+            else
+              tt(iz,n)=tt(iz,n)/prespoti(iz)
+            endif
+            qq(iz,n)=qsnd(i-1,m)+(qsnd(i,m)-qsnd(i-1,m))*coef
+            uu(iz,n)=usnd(i-1,m)+(usnd(i,m)-usnd(i-1,m))*coef
+            vv(iz,n)=vsnd(i-1,m)+(vsnd(i,m)-vsnd(i-1,m))*coef
            goto 11
           endif
         end do
       else
         do i = 2,nzsnd
-          if(prespoti(iz).ge.psnd(i,m)) then
-           coef = (prespoti(iz)-psnd(i-1,m))/(psnd(i,m)-psnd(i-1,m))
-           tt(iz,n)=tsnd(i-1,m)+(tsnd(i,m)-tsnd(i-1,m))*coef
-           tt(iz,n)=tt(iz,n)/prespoti(iz)
-           qq(iz,n)=qsnd(i-1,m)+(qsnd(i,m)-qsnd(i-1,m))*coef
-           uu(iz,n)=usnd(i-1,m)+(usnd(i,m)-usnd(i-1,m))*coef
-           vv(iz,n)=vsnd(i-1,m)+(vsnd(i,m)-vsnd(i-1,m))*coef
+          if(pres(iz).ge.psnd(i,m)) then
+            coef = (pres(iz)-psnd(i-1,m))/(psnd(i,m)-psnd(i-1,m))
+            tt(iz,n)=tsnd(i-1,m)+(tsnd(i,m)-tsnd(i-1,m))*coef
+            tt(iz,n)=tt(iz,n)/prespoti(iz)
+            qq(iz,n)=qsnd(i-1,m)+(qsnd(i,m)-qsnd(i-1,m))*coef
+            uu(iz,n)=usnd(i-1,m)+(usnd(i,m)-usnd(i-1,m))*coef
+            vv(iz,n)=vsnd(i-1,m)+(vsnd(i,m)-vsnd(i-1,m))*coef
            goto 11
           endif
         end do
